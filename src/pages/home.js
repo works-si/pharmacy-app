d00676487f568fbdf4882717d0c815350bae74d3
paschoal <jaqueline.paschoal@scaffoldeducation.com.br>
2019-08-24 16:26:32 -0300

    create component list

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,6 +8,7 @@ import {
   Button
 } from "react-native";
 import LogoHeader from "../images/logo-header.png";
+import Menu from "../images/menu.png";
 
 class LogoTitle extends Component {
   render() {
@@ -19,13 +20,33 @@ class LogoTitle extends Component {
   }
 }
 
-export default class Login extends Component {
+export default class Home extends Component {
+  state = {
+    medicines: [
+      {
+        title: "bla",
+        hour: "12/12"
+      },
+      {
+        title: "bla",
+        hour: "12/12"
+      },
+      {
+        title: "bla",
+        hour: "12/12"
+      }
+    ]
+  };
+
   static navigationOptions = ({ navigation }) => {
     return {
       headerTitle: <LogoTitle />,
       headerLeft: (
-        <TouchableOpacity onPress={() => navigation.openDrawer()}>
-          <Text>Open Drawer</Text>
+        <TouchableOpacity
+          onPress={() => navigation.openDrawer()}
+          style={styles.menu}
+        >
+          <Image source={Menu} />
         </TouchableOpacity>
       ),
       headerStyle: {
@@ -36,8 +57,69 @@ export default class Login extends Component {
   };
 
   render() {
-    return <View />;
+    return (
+      <View style={styles.content}>
+        <Text style={styles.title}> Meus medicamentos</Text>
+        {this.state.medicines.map(() => {
+          return (
+            <View style={styles.contentList}>
+              <TouchableOpacity style={styles.itemList}>
+                <Text style={styles.titleList}>Furosemida 50mg</Text>
+                <Text>12/12 hrs</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        })}
+      </View>
+    );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  menu: {
+    paddingLeft: 20
+  },
+  content: {
+    backgroundColor: "#e5e5e5",
+    height: 1000
+  },
+  title: {
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    fontSize: 16,
+    lineHeight: 15,
+    color: "#585858",
+    marginTop: 30,
+    marginLeft: 20,
+    marginBottom: 20
+  },
+  contentList: {
+    alignItems: "center",
+    marginBottom: 15
+  },
+  itemList: {
+    backgroundColor: "#fff",
+    width: "90%",
+    padding: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5
+  },
+  titleList: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: "rgba(37, 37, 37, 0.87)"
+  },
+  subtitleList: {
+    fontSize: 12,
+    letterSpacing: 10,
+    lineHeight: 20,
+    color: "rgba(0, 0, 0, 0.5438)"
+  }
+});
